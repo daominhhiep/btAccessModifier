9622384536096da59cf09c74d75626cf7222dd4c
admin <daominhhiep97@gmail.com>
2020-07-09 11:18:08 +0700

    [Bài tập] Access modifier

---
+++ src/com/company/TestCricle.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestCricle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3);
+        System.out.println("Area : " + circle.getArea() + " Radius: " + circle.getRadius());
+    }
+}
